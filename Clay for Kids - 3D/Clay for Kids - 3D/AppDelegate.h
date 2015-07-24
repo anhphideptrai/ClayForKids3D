af4c3cc64c483d78b82564e3ae0d98f9d197d585
Phi Nguyen <phi.nguyen@infonam.com>
2015-07-24 15:03:17 +0700

    - Init project

---
+++ Clay for Kids - 3D/Clay for Kids - 3D/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clay for Kids - 3D
+//
+//  Created by Phi Nguyen on 7/24/15.
+//  Copyright (c) 2015 Thien Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clay for Kids - 3D/Clay for Kids - 3D/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clay for Kids - 3D
+//
+//  Created by Phi Nguyen on 7/24/15.
+//  Copyright (c) 2015 Thien Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
